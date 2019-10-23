7cb912397bb6b70224732bdd991b936f6fc31a15
Wan <wwanxh@gmail.com>
2019-10-23 15:18:08 -0700

    added numpy examples and matplotlib examples

---
+++ matplotlib_practice.py
@@ -0,0 +1,36 @@
+# Matplotlib is a plotting library. In this section give a brief introduction to the matplotlib.pyplot
+# module, which provides a plotting system similar to that of MATLAB.
+# Author: Shawn Jin
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Compute the x and y coordinates for points on sine and cosine curves
+x = np.arange(0, 3 * np.pi, 0.1)
+y_sin = np.sin(x)
+y_cos = np.cos(x)
+
+# Plot the points using matplotlib
+plt.plot(x, y_sin)
+plt.plot(x, y_cos)
+plt.xlabel('x axis label')
+plt.ylabel('y axis label')
+plt.title('Sine and Cosine')
+plt.legend(['Sine', 'Cosine'])
+plt.show()
+
+# Set up a subplot grid that has height 2 and width 1,
+# and set the first such subplot as active.
+plt.subplot(2, 1, 1)
+
+# Make the first plot
+plt.plot(x, y_sin)
+plt.title('Sine')
+
+# Set the second subplot as active, and make the second plot.
+plt.subplot(2, 1, 2)
+plt.plot(x, y_cos)
+plt.title('Cosine')
+
+# Show the figure.
+plt.show()
